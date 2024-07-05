1e6adfe621b9b88805bb3aa0b063c6e714f79248
Vanessa Ilombe <vnsssitailombe@googlemail.com>
2024-07-05 00:12:44 +0000

    Reverted to the previous version

--- about/views.py
+++ about/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+
 # Create your views here.
 
 def about(request):
+
+    if request.method == "POST":
+        contact_form = ContactForm(data=request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
+
     """
     Renders the About page
     """
@@ -15,3 +24,4 @@ def about(request):
 
     )
    
+
